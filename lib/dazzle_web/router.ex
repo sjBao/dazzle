78ce4dba5e4d0127dcbe4522322b9bdab134b175
sjbao <supuilam@gmail.com>
2024-08-13 20:27:15 -0400

    Add routes for user show and show/edit

--- lib/dazzle_web/router.ex
+++ lib/dazzle_web/router.ex
@@ -23,6 +23,10 @@ defmodule DazzleWeb.Router do
 
     live "/users", UserLive.Index, :index
     live "/users/new", UserLive.Index, :new
+    live "/users/:id/edit", UserLive.Index, :edit
+    live "/users/:id", UserLive.Show, :show
+    live "/users/:id/show/edit", UserLive.Show, :edit
+
 
   end
 
