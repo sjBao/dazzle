40c4e2a9297148d9cb67316c8e907ddeeb4b55ae
sjbao <supuilam@gmail.com>
2024-09-14 16:46:15 -0400

    User assigns instead of hardcoded values for /sync

--- lib/dazzle_web/controllers/live/sync_live/index.ex
+++ lib/dazzle_web/controllers/live/sync_live/index.ex
@@ -1,9 +1,16 @@
 defmodule DazzleWeb.SyncLive.Index do
   use DazzleWeb, :live_view
 
-  def render(assigns) do
-    ~H"""
-      Hello Bro
-    """
+  def mount(%{"user" => user, "count" => count}, _session, socket) do
+    # Don't use string to atom for user input in production code!
+    # atoms are finite and can be exhausted by an attacker.
+    user = String.to_atom(user)
+    count = to_string(count)
+
+    # Not production code! For educational purposes only
+    # Pubsub is the correct tool for this application
+    Process.register(self(), user)
+
+    {:ok, assign(socket, user: user, count: count)}
   end
 end
